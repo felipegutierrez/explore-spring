7108fdcc0e22633f2f94b9e6db28c225bb699c39
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2021-03-08 09:31:20 +0100

    first commit. Imported spring demo project from https://start.spring.io/

---
+++ src/main/java/com/github/felipegutierrez/explore/spring/ExploreSpringApplication.java
@@ -0,0 +1,13 @@
+package com.github.felipegutierrez.explore.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreSpringApplication.class, args);
+	}
+
+}
