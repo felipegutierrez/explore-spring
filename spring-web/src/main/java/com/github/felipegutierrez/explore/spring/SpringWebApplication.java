64a674f005193adfda22c574db10adae370b5578
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2021-03-10 10:53:34 +0100

    first spring web rest with a controller [skip ci]

--- spring-web/src/main/java/com/github/felipegutierrez/explore/spring/SpringWebApplication.java
+++ spring-web/src/main/java/com/github/felipegutierrez/explore/spring/SpringWebApplication.java
@@ -1,13 +1,26 @@
 package com.github.felipegutierrez.explore.spring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class SpringWebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringWebApplication.class, args);
-	}
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringWebApplication.class);
 
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                SpringApplication.run(SpringWebApplication.class, args);
+        LOGGER.info("use:");
+        LOGGER.info("http://localhost:8080/books");
+
+        Arrays.stream(applicationContext.getBeanDefinitionNames()).forEach(name ->
+                LOGGER.info(name)
+        );
+    }
 }
---
+++ spring-web/src/main/java/com/github/felipegutierrez/explore/spring/controller/BookController.java
@@ -0,0 +1,25 @@
+package com.github.felipegutierrez.explore.spring.controller;
+
+import com.github.felipegutierrez.explore.spring.beans.Book;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class BookController {
+
+    private final List<Book> bookList;
+
+    public BookController() {
+        this.bookList = new ArrayList<Book>();
+        this.bookList.add(new Book(1L, "The portrait of Dorian Gray", "Oscar Wilde"));
+        this.bookList.add(new Book(2L, "To be or not to be", "William Shakespeare"));
+    }
+
+    @GetMapping("/books")
+    public List<Book> getAllBooks() {
+        return this.bookList;
+    }
+}
