05d839b7db26c64e8d259e2d5b2f38ea33fb4cb9
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2021-03-12 15:49:32 +0100

    Flux using virtual time scheduler [skip ci]

--- spring-web-react/src/test/java/com/github/felipegutierrez/explore/spring/flux/FluxAndMonoStreamsTest.java
+++ spring-web-react/src/test/java/com/github/felipegutierrez/explore/spring/flux/FluxAndMonoStreamsTest.java
@@ -2,12 +2,10 @@ package com.github.felipegutierrez.explore.spring.flux;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
-import reactor.core.scheduler.Scheduler;
-import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
+import reactor.test.scheduler.VirtualTimeScheduler;
 
 import java.time.Duration;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class FluxAndMonoStreamsTest {
 
@@ -35,11 +33,23 @@ public class FluxAndMonoStreamsTest {
 
     @Test
     void testCreateFluxStreamWithMapAndDelay() {
-        Flux<Double> streamLongFlux = myFluxAndMonoStreams.createFluxDoubleWithDelay(3, 1);
+        Flux<Double> streamLongFlux = myFluxAndMonoStreams.createFluxDoubleWithDelay(4, 1);
 
         StepVerifier.create(streamLongFlux)
                 .expectSubscription()
-                .expectNext(0.0, 1.0, 2.0)
+                .expectNext(0.0, 1.0, 2.0, 3.0)
+                .verifyComplete();
+    }
+
+    @Test
+    void testCreateFluxStreamWithMapAndDelayWithVirtualTime() {
+        VirtualTimeScheduler.getOrSet();
+        Flux<Double> streamLongFlux = myFluxAndMonoStreams.createFluxDoubleWithDelay(10, 1);
+
+        StepVerifier.withVirtualTime(() -> streamLongFlux)
+                .expectSubscription()
+                .thenAwait(Duration.ofSeconds(10))
+                .expectNext(0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0)
                 .verifyComplete();
     }
 
