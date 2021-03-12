618127237c407b455a25b06ff9706cefe2f43a0b
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2021-03-12 18:35:27 +0100

    comments [skip ci]

--- spring-web-react/src/test/java/com/github/felipegutierrez/explore/spring/flux/FluxAndMonoStreamsTest.java
+++ spring-web-react/src/test/java/com/github/felipegutierrez/explore/spring/flux/FluxAndMonoStreamsTest.java
@@ -2,10 +2,13 @@ package com.github.felipegutierrez.explore.spring.flux;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
 import reactor.test.scheduler.VirtualTimeScheduler;
 
 import java.time.Duration;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class FluxAndMonoStreamsTest {
 
@@ -53,27 +56,31 @@ public class FluxAndMonoStreamsTest {
                 .verifyComplete();
     }
 
-    /*
     @Test
     void testCreateFluxStreamWithMapAndVerifyDelay() {
+        // VirtualTimeScheduler.getOrSet();
         Flux<Double> streamLongFlux = myFluxAndMonoStreams
                 .createFluxDoubleWithDelay(4, 1);
 
+/*
         Scheduler scheduler = Schedulers.newSingle("test");
         AtomicInteger incrementer = new AtomicInteger();
-
-        StepVerifier
-                .withVirtualTime(() -> streamLongFlux
-                        .subscribeOn(scheduler)
-                        .doOnNext(value -> incrementer.incrementAndGet())
-                )
+        StepVerifier.withVirtualTime(() -> streamLongFlux
+                .subscribeOn(scheduler)
+                .doOnNext(value -> incrementer.incrementAndGet())
+        )
                 .expectSubscription()
-                .thenAwait(Duration.ofSeconds(4))
-                // .thenConsumeWhile(value -> true)
-                // .expectNextCount(4)
-                // .expectNext(0.0, 1.0, 2.0, 3.0)
-                .verifyComplete()
-        ;
+                .thenAwait(Duration.ofSeconds(2))
+                .expectNext(0.0, 1.0, 2.0, 3.0)
+                .verifyComplete();
+         // for the first block of code, if you change `.thenAwait(Duration.ofSeconds(4))` for `.thenAwait(Duration.ofSeconds(1))` it also give green bar. I was expecting a red bar because the computation takes 4 seconds.
+*/
+/*
+        StepVerifier.withVirtualTime(() -> streamLongFlux)
+                .expectSubscription()
+                .thenAwait(Duration.ofSeconds(2))
+                .expectNext(0.0, 1.0, 2.0, 3.0)
+                .verifyComplete();
+*/
     }
-     */
 }
