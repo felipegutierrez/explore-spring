c194a4c66c692030e817a868d90bd81ada370726
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2021-03-12 13:32:07 +0100

    create Flux with backpressure [skip ci]

---
+++ spring-web-react/src/main/java/com/github/felipegutierrez/explore/spring/flux/FluxMonoBackpressure.java
@@ -0,0 +1,14 @@
+package com.github.felipegutierrez.explore.spring.flux;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+
+@Slf4j
+public class FluxMonoBackpressure {
+
+    public Flux<Integer> createFluxFinite(Integer start, Integer end) {
+        return Flux
+                .range(start, end)
+                .log();
+    }
+}
