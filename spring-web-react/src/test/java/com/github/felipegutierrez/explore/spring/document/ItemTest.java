eb1e096f9ad92fcde746f9aae488b82934673145
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2021-03-15 16:03:01 +0100

    created MongoDB Document Item using Lombok and its unit test [skip ci]

---
+++ spring-web-react/src/test/java/com/github/felipegutierrez/explore/spring/document/ItemTest.java
@@ -0,0 +1,27 @@
+package com.github.felipegutierrez.explore.spring.document;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ItemTest {
+
+    @Test
+    public void testItemGetters() {
+        Item item = new Item("id", "desc", 45.6);
+        assertEquals(item.getId(), "id");
+        assertEquals(item.getDescription(), "desc");
+        assertEquals(item.getPrice(), Double.valueOf(45.6));
+    }
+
+    @Test
+    public void testItemSetters() {
+        Item item = new Item();
+        item.setId("id");
+        item.setDescription("desc");
+        item.setPrice(45.6);
+        assertEquals(item.getId(), "id");
+        assertEquals(item.getDescription(), "desc");
+        assertEquals(item.getPrice(), Double.valueOf(45.6));
+    }
+}
