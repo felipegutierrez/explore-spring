50967ed4b3a4dfc493badb0975df9fa6734424bd
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2021-03-16 16:27:37 +0100

    functional router for the server end-point getAllItems other options [skip ci]

---
+++ spring-web-react/src/test/java/com/github/felipegutierrez/explore/spring/handler/ItemsHandlerTest.java
@@ -0,0 +1,96 @@
+package com.github.felipegutierrez.explore.spring.handler;
+
+import com.github.felipegutierrez.explore.spring.document.Item;
+import com.github.felipegutierrez.explore.spring.repository.ItemReactiveRepository;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static com.github.felipegutierrez.explore.spring.util.ItemConstants.ITEM_FUNCTIONAL_ENDPOINT_V1;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@DirtiesContext
+@AutoConfigureWebTestClient
+@ActiveProfiles("test")
+public class ItemsHandlerTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Autowired
+    ItemReactiveRepository itemReactiveRepository;
+
+    public List<Item> data() {
+        return Arrays.asList(
+                new Item(null, "Samsung TV", 399.99),
+                new Item(null, "LG TV", 369.99),
+                new Item("gTvId", "Google TV", 439.99),
+                new Item("hardcodeID", "Sony TV", 424.99));
+    }
+
+    @BeforeAll
+    public void setUp() {
+        itemReactiveRepository.deleteAll()
+                .thenMany(Flux.fromIterable(data()))
+                .flatMap(itemReactiveRepository::save)
+                .doOnNext(item -> System.out.println("inserted item: " + item))
+                .blockLast();
+    }
+
+    @Test
+    @Order(1)
+    public void getAllItemsApproach01() {
+        webTestClient.get().uri(ITEM_FUNCTIONAL_ENDPOINT_V1)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(Item.class)
+                .hasSize(data().size());
+    }
+
+    @Test
+    @Order(2)
+    public void getAllItemsApproach02() {
+        webTestClient.get().uri(ITEM_FUNCTIONAL_ENDPOINT_V1)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(Item.class)
+                .hasSize(data().size())
+                .consumeWith(response -> {
+                    List<Item> items = response.getResponseBody();
+                    items.forEach(item -> assertTrue(item.getId() != null));
+                });
+    }
+
+    @Test
+    @Order(3)
+    public void getAllItemsApproach03() {
+        Flux<Item> itemsFlux = webTestClient.get().uri(ITEM_FUNCTIONAL_ENDPOINT_V1)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .returnResult(Item.class)
+                .getResponseBody();
+        StepVerifier.create(itemsFlux.log("getAllItemsApproach03"))
+                .expectSubscription()
+                .expectNextCount(data().size())
+                .verifyComplete();
+    }
+}
