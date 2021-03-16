85932747187558f15a4f203718a028299d861b3c
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2021-03-16 12:24:43 +0100

    inserting data into MongoDB with CommandLineRunner and retrieving from the browser [skip ci]

---
+++ spring-web-react/src/main/java/com/github/felipegutierrez/explore/spring/initialize/ItemDataInitializer.java
@@ -0,0 +1,41 @@
+package com.github.felipegutierrez.explore.spring.initialize;
+
+import com.github.felipegutierrez.explore.spring.document.Item;
+import com.github.felipegutierrez.explore.spring.repository.ItemReactiveRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+@Slf4j
+public class ItemDataInitializer implements CommandLineRunner {
+
+    @Autowired
+    ItemReactiveRepository itemReactiveRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        initialDataSetup();
+    }
+
+    public List<Item> data() {
+        return Arrays.asList(
+                new Item(null, "Samsung TV", 399.99),
+                new Item(null, "LG TV", 369.99),
+                new Item(null, "Google TV", 439.99),
+                new Item("hardcodeID", "Sony TV", 424.99));
+    }
+
+    private void initialDataSetup() {
+        itemReactiveRepository.deleteAll()
+                .thenMany(Flux.fromIterable(data()))
+                .flatMap(itemReactiveRepository::save)
+                .thenMany(itemReactiveRepository.findAll())
+                .subscribe(item -> log.info("Item inserted from CommandLineRunner: {}", item));
+    }
+}
