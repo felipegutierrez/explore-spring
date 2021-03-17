e71569593f995267081104bc3f05b2f62eb8bf78
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2021-03-17 09:54:21 +0100

    implemented web reactive client to connect to the web reactive server [skip ci]

--- spring-web-react-client/src/main/java/com/github/felipegutierrez/explore/spring/controller/ItemClientController.java
+++ spring-web-react-client/src/main/java/com/github/felipegutierrez/explore/spring/controller/ItemClientController.java
@@ -1,7 +1,30 @@
 package com.github.felipegutierrez.explore.spring.controller;
 
+import com.github.felipegutierrez.explore.spring.domain.Item;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+
+import static com.github.felipegutierrez.explore.spring.util.ItemConstants.ITEM_ENDPOINT_V1;
 
 @RestController
 public class ItemClientController {
+
+    WebClient webClient = WebClient.create("http://localhost:8080");
+
+    @GetMapping("/client/retrieve")
+    public Flux<Item> getAllItemsUsingRetrieve() {
+        return webClient.get().uri(ITEM_ENDPOINT_V1)
+                .retrieve()
+                .bodyToFlux(Item.class)
+                .log("Items in client project retrieve: ");
+    }
+
+    @GetMapping("/client/exchange")
+    public Flux<Item> getAllItemsUsingExchange() {
+        return webClient.get().uri(ITEM_ENDPOINT_V1)
+                .exchangeToFlux(clientResponse -> clientResponse.bodyToFlux(Item.class))
+                .log("Items in client project exchange: ");
+    }
 }
---
+++ spring-web-react-client/src/main/java/com/github/felipegutierrez/explore/spring/util/ItemConstants.java
@@ -0,0 +1,6 @@
+package com.github.felipegutierrez.explore.spring.util;
+
+public class ItemConstants {
+    public static final String ITEM_ENDPOINT_V1 = "/v1/item";
+    public static final String ITEM_FUNCTIONAL_ENDPOINT_V1 = "/v1/func/item";
+}
