0c4cca1227415c42aa58c55964c08e7d5529a32e
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2021-03-17 09:32:57 +0100

    created new module for the spring web client reactive [skip ci]

---
+++ spring-web-react-client/src/main/java/com/github/felipegutierrez/explore/spring/SpringWebReactClientApplication.java
@@ -0,0 +1,13 @@
+package com.github.felipegutierrez.explore.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebReactClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebReactClientApplication.class, args);
+	}
+
+}
---
+++ spring-web-react-client/src/main/java/com/github/felipegutierrez/explore/spring/controller/ItemClientController.java
@@ -0,0 +1,7 @@
+package com.github.felipegutierrez.explore.spring.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ItemClientController {
+}
