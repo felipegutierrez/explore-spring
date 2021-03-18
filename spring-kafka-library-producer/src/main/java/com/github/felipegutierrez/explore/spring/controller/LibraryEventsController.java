53fd476ad195abe03292f965d721117461a0eb40
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2021-03-18 16:24:10 +0100

    injecting the LibraryEventProducer with KafkaTemplate in the LibraryEventsController [skip ci]

--- spring-kafka-library-producer/src/main/java/com/github/felipegutierrez/explore/spring/controller/LibraryEventsController.java
+++ spring-kafka-library-producer/src/main/java/com/github/felipegutierrez/explore/spring/controller/LibraryEventsController.java
@@ -1,7 +1,10 @@
 package com.github.felipegutierrez.explore.spring.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.github.felipegutierrez.explore.spring.domain.LibraryEvent;
+import com.github.felipegutierrez.explore.spring.producer.LibraryEventProducer;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,19 +17,25 @@ import static com.github.felipegutierrez.explore.spring.util.LibraryConstants.LI
 @Slf4j
 public class LibraryEventsController {
 
+    @Autowired
+    LibraryEventProducer libraryEventProducer;
+
     /**
      * test on CLI using:
      * "http POST http://localhost:8080/v1/libraryevent < spring-kafka-library-producer/src/main/resources/static/libraryEvent-00.json"
+     * <p>
+     * consume on the Kafka broker using:
+     * "./bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic library-events --from-beginning --property "print.key=true""
      *
      * @param libraryEvent
      * @return
      */
     @PostMapping(LIBRARY_V1_ENDPOINT)
-    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) {
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
         log.info("received request: {}", libraryEvent);
 
         // invoke the kafka producer
-
+        libraryEventProducer.sendLibraryEvent(libraryEvent);
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(libraryEvent);
