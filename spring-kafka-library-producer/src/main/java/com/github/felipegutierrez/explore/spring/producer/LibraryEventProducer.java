00dbfc2e1390deca162afa7210e1559fd98b8261
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2021-03-18 17:32:08 +0100

    added header to the Kafka messages [skip ci]

--- spring-kafka-library-producer/src/main/java/com/github/felipegutierrez/explore/spring/producer/LibraryEventProducer.java
+++ spring-kafka-library-producer/src/main/java/com/github/felipegutierrez/explore/spring/producer/LibraryEventProducer.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.felipegutierrez.explore.spring.domain.LibraryEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.Header;
+import org.apache.kafka.common.header.internals.RecordHeader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -81,7 +84,10 @@ public class LibraryEventProducer {
     }
 
     private ProducerRecord<Integer, String> buildProducerRecord(String topic, Integer key, String value) {
-        return new ProducerRecord<Integer, String>(topic, null, key, value, null);
+
+        List<Header> recordHeaders = List.of(new RecordHeader("event-source", "scanner".getBytes()));
+
+        return new ProducerRecord<Integer, String>(topic, null, key, value, recordHeaders);
     }
 
     private void handleSuccess(Integer key, String value, SendResult<Integer, String> result) {
