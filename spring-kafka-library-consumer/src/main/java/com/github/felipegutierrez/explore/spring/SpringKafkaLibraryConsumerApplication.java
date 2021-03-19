14fb92f73617d007d147e5b7f74f2b8dca787cb7
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2021-03-19 16:47:41 +0100

    example of commands [skip ci]

--- spring-kafka-library-consumer/src/main/java/com/github/felipegutierrez/explore/spring/SpringKafkaLibraryConsumerApplication.java
+++ spring-kafka-library-consumer/src/main/java/com/github/felipegutierrez/explore/spring/SpringKafkaLibraryConsumerApplication.java
@@ -8,6 +8,17 @@ public class SpringKafkaLibraryConsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringKafkaLibraryConsumerApplication.class, args);
+
+		System.out.println("Start the Kafka broker");
+		System.out.println("./bin/zookeeper-server-start.sh config/zookeeper.properties");
+		System.out.println("./bin/kafka-server-start.sh config/server.properties");
+		System.out.println("./bin/kafka-server-start.sh config/server-01.properties");
+		System.out.println("./bin/kafka-server-start.sh config/server-02.properties");
+		System.out.println();
+		System.out.println("try out commands:");
+		System.out.println("http POST http://localhost:8080/v1/libraryevent < spring-kafka-library-producer/src/main/resources/static/libraryEvent-00.json");
+		System.out.println("http PUT  http://localhost:8080/v1/libraryevent < spring-kafka-library-producer/src/main/resources/static/libraryEvent-01.json");
+		System.out.println("http PUT  http://localhost:8080/v1/libraryevent < spring-kafka-library-producer/src/main/resources/static/libraryEvent-02.json");
 	}
 
 }
