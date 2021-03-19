ecbe116f1ce05c5b86ec2e4b15527b4a970a6400
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2021-03-19 18:07:14 +0100

    added library to rebalance spring+kafka consumers

--- spring-kafka-library-consumer/src/main/java/com/github/felipegutierrez/explore/spring/SpringKafkaLibraryConsumerApplication.java
+++ spring-kafka-library-consumer/src/main/java/com/github/felipegutierrez/explore/spring/SpringKafkaLibraryConsumerApplication.java
@@ -15,6 +15,11 @@ public class SpringKafkaLibraryConsumerApplication {
 		System.out.println("./bin/kafka-server-start.sh config/server-01.properties");
 		System.out.println("./bin/kafka-server-start.sh config/server-02.properties");
 		System.out.println();
+		System.out.println("alunching application from command line");
+		System.out.println("java -jar -Dserver.port=8081 build/libs/spring-kafka-library-consumer-0.0.1.jar");
+		System.out.println("java -jar -Dserver.port=8082 build/libs/spring-kafka-library-consumer-0.0.1.jar");
+		System.out.println("java -jar -Dserver.port=8083 build/libs/spring-kafka-library-consumer-0.0.1.jar");
+		System.out.println();
 		System.out.println("try out commands:");
 		System.out.println("http POST http://localhost:8080/v1/libraryevent < spring-kafka-library-producer/src/main/resources/static/libraryEvent-00.json");
 		System.out.println("http PUT  http://localhost:8080/v1/libraryevent < spring-kafka-library-producer/src/main/resources/static/libraryEvent-01.json");
