77af87211e41bb28753da271b305051719378157
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2021-03-22 19:02:53 +0100

    instructions to generate ssl certifiate to Kafka cluster [skip ci]

---
+++ spring-kafka-library-producer/ssl/setup-ssl.sh
@@ -0,0 +1,31 @@
+
+
+# 1. Generating the KeyStore
+keytool -keystore server.keystore.jks -alias localhost -validity 365 -genkey -keyalg RSA
+keytool -list -v -keystore server.keystore.jks
+
+# 2. Generating CA
+openssl req -new -x509 -keyout ca-key -out ca-cert -days 365 -subj "/CN=local-security-CA"
+
+# 3. Certificate Signing Request(CSR)
+keytool -keystore server.keystore.jks -alias localhost -certreq -file cert-file
+
+# 4. Signing the certificate
+openssl x509 -req -CA ca-cert -CAkey ca-key -in cert-file -out cert-signed -days 365 -CAcreateserial -passin pass:password
+keytool -printcert -v -file cert-signed
+
+# 5. Adding the Signed Cert in to the KeyStore file
+keytool -keystore server.keystore.jks -alias CARoot -import -file ca-cert
+keytool -keystore server.keystore.jks -alias localhost -import -file cert-signed
+
+# 6. edit the kafka server config/server.property
+ssl.keystore.location=<location>/server.keystore.jks
+ssl.keystore.password=password
+ssl.key.password=password
+ssl.endpoint.identification.algorithm=
+
+
+# 7. Generate the TrustStore
+keytool -keystore client.truststore.jks -alias CARoot -import -file ca-cert
+
+
