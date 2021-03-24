395c31bcf3e8f84bc2a2d63f1e5b38ac0da7b973
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2021-03-24 15:11:50 +0100

    created reactive rest controller with POST and PUT for the Spring+Kafka producer [skip ci]

--- spring-kafka-library-producer/src/main/java/com/github/felipegutierrez/explore/spring/controller/LibraryEventsReactController.java
+++ spring-kafka-library-producer/src/main/java/com/github/felipegutierrez/explore/spring/controller/LibraryEventsReactController.java
@@ -8,20 +8,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.kafka.support.SendResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeoutException;
-
-import static com.github.felipegutierrez.explore.spring.util.LibraryConstants.*;
+import static com.github.felipegutierrez.explore.spring.util.LibraryConstants.LIBRARY_ERROR_ID_NULL;
+import static com.github.felipegutierrez.explore.spring.util.LibraryConstants.LIBRARY_REACT_V1_ENDPOINT;
 
 /**
  * Testing based on the project:
- *
+ * <p>
  * https://github.com/svgagan/server-sent-events
  */
 // @EnableAutoConfiguration
@@ -52,6 +52,39 @@ public class LibraryEventsReactController {
 
         return Mono.just(libraryEvent)
                 .map(event -> new ResponseEntity<>(event, HttpStatus.CREATED))
+                .log();
+    }
+
+    /**
+     * test on CLI using:
+     * "http POST http://localhost:8080/v1/react/libraryevent < spring-kafka-library-producer/src/main/resources/static/libraryEvent-00.json"
+     * "http PUT  http://localhost:8080/v1/react/libraryevent < spring-kafka-library-producer/src/main/resources/static/libraryEvent-01.json"
+     * "http PUT  http://localhost:8080/v1/react/libraryevent < spring-kafka-library-producer/src/main/resources/static/libraryEvent-02.json"
+     *
+     * <p>
+     * consume on the Kafka broker using:
+     * "./bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic library-events --from-beginning --property "print.key=true""
+     *
+     * @param libraryEvent
+     * @return
+     * @throws JsonProcessingException
+     */
+    @PutMapping(LIBRARY_REACT_V1_ENDPOINT)
+    public Mono<ResponseEntity> putLibraryEvent(@RequestBody @Valid LibraryEvent libraryEvent) throws JsonProcessingException {
+        log.info("received PUT reactive request: {}", libraryEvent);
+
+        if (libraryEvent.getLibraryEventId() == null) {
+            return Mono
+                    .just(new ResponseEntity(LIBRARY_ERROR_ID_NULL, HttpStatus.BAD_REQUEST))
+                    .log();
+        }
+        libraryEvent.setLibraryEventType(LibraryEventType.UPDATE);
+
+        // invoke the kafka producer and send message asynchronously
+        libraryEventProducer.sendLibraryEventWithProducerRecord(libraryEvent);
+
+        return Mono.just(libraryEvent)
+                .map(event -> new ResponseEntity(event, HttpStatus.OK))
                 .log();
     }
 }
