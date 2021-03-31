3e3ee134bc5e16a2769b5769b2852fe9a80bfa82
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2021-03-31 14:25:55 +0200

    created the consumer for the Spring cloud + Kafka stream with HTTP GET response method [skip ci]

--- spring-kafka-stream/src/main/java/com/github/felipegutierrez/explore/spring/handlers/WordListenerHandler.java
+++ spring-kafka-stream/src/main/java/com/github/felipegutierrez/explore/spring/handlers/WordListenerHandler.java
@@ -11,7 +11,8 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.time.Duration;
+import java.util.LinkedList;
+import java.util.Queue;
 
 @Slf4j
 @Component
@@ -19,20 +20,21 @@ public class WordListenerHandler {
 
     private final String WORDS_STREAMING_OUTPUT_TOPIC_NAME = "streaming-words-output-topic";
 
-    // @Autowired
-    // WordListenerService wordListenerService;
+    Queue<String> stringStream = new LinkedList<String>();
 
     public Mono<ServerResponse> fluxWordCountStream(ServerRequest serverRequest) {
+
         return ServerResponse.ok()
                 .contentType(MediaType.APPLICATION_STREAM_JSON)
-                // .body(wordListenerService::process)
-                .body(Flux.interval(Duration.ofSeconds(1)).log(), Long.class);
+                .body(Flux.fromStream(stringStream.stream()), String.class);
+        // .body(Flux.interval(Duration.ofSeconds(1)).log(), Long.class);
     }
 
     @KafkaListener(topics = {WORDS_STREAMING_OUTPUT_TOPIC_NAME})
     public void onMessage(ConsumerRecord<String, String> consumerRecord) throws JsonProcessingException {
         log.info("ConsumerRecord received: {}", consumerRecord);
 
-        // libraryEventService.processLibraryEvent(consumerRecord);
+        String message = consumerRecord.key() + " : " + consumerRecord.value() + "\n";
+        stringStream.add(message);
     }
 }
