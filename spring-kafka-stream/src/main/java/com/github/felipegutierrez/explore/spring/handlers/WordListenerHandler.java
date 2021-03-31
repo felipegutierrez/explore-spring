dea16b8dde95185920c0bd2152d0af61084a0b00
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2021-03-31 17:03:54 +0200

    :bug: fixed the stream word count to retrieve on HTTP GET request [skip ci]

--- spring-kafka-stream/src/main/java/com/github/felipegutierrez/explore/spring/handlers/WordListenerHandler.java
+++ spring-kafka-stream/src/main/java/com/github/felipegutierrez/explore/spring/handlers/WordListenerHandler.java
@@ -8,11 +8,8 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.util.LinkedList;
-import java.util.Queue;
+import reactor.core.publisher.Sinks;
 
 @Slf4j
 @Component
@@ -20,14 +17,13 @@ public class WordListenerHandler {
 
     private final String WORDS_STREAMING_OUTPUT_TOPIC_NAME = "streaming-words-output-topic";
 
-    Queue<String> stringStream = new LinkedList<String>();
+    Sinks.Many<String> replaySink = Sinks.many().multicast().onBackpressureBuffer();
 
     public Mono<ServerResponse> fluxWordCountStream(ServerRequest serverRequest) {
 
         return ServerResponse.ok()
                 .contentType(MediaType.APPLICATION_STREAM_JSON)
-                .body(Flux.fromStream(stringStream.stream()), String.class);
-        // .body(Flux.interval(Duration.ofSeconds(1)).log(), Long.class);
+                .body(replaySink.asFlux().log(), String.class);
     }
 
     @KafkaListener(topics = {WORDS_STREAMING_OUTPUT_TOPIC_NAME})
@@ -35,6 +31,7 @@ public class WordListenerHandler {
         log.info("ConsumerRecord received: {}", consumerRecord);
 
         String message = consumerRecord.key() + " : " + consumerRecord.value() + "\n";
-        stringStream.add(message);
+
+        replaySink.emitNext(message, Sinks.EmitFailureHandler.FAIL_FAST);
     }
 }
