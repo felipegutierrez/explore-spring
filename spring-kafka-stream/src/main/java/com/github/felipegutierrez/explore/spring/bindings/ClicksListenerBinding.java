4f5ce08ba9692935771b3a58bfb0c42cd486e3d9
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2021-04-02 13:01:52 +0200

    created binding and service for GlobalKTable to compute click from KStream using broadcast strategy [skip ci]

--- spring-kafka-stream/src/main/java/com/github/felipegutierrez/explore/spring/bindings/ClicksListenerBinding.java
+++ spring-kafka-stream/src/main/java/com/github/felipegutierrez/explore/spring/bindings/ClicksListenerBinding.java
@@ -1,4 +1,16 @@
 package com.github.felipegutierrez.explore.spring.bindings;
 
+import com.github.felipegutierrez.explore.spring.model.AdClick;
+import com.github.felipegutierrez.explore.spring.model.AdInventories;
+import org.apache.kafka.streams.kstream.GlobalKTable;
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.cloud.stream.annotation.Input;
+
 public interface ClicksListenerBinding {
+
+    @Input("inventories-channel")
+    GlobalKTable<String, AdInventories> inventoryInputStream();
+
+    @Input("clicks-channel")
+    KStream<String, AdClick> clickInputStream();
 }
--- spring-kafka-stream/src/main/java/com/github/felipegutierrez/explore/spring/services/ClickListenerService.java
+++ spring-kafka-stream/src/main/java/com/github/felipegutierrez/explore/spring/services/ClickListenerService.java
@@ -1,13 +1,24 @@
 package com.github.felipegutierrez.explore.spring.services;
 
 import com.github.felipegutierrez.explore.spring.bindings.ClicksListenerBinding;
+import com.github.felipegutierrez.explore.spring.model.AdClick;
+import com.github.felipegutierrez.explore.spring.model.AdInventories;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.streams.kstream.GlobalKTable;
+import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 @EnableBinding(ClicksListenerBinding.class)
 public class ClickListenerService {
+    @StreamListener
+    public void process(@Input("inventories-channel") GlobalKTable<String, AdInventories> inventory,
+                        @Input("clicks-channel") KStream<String, AdClick> click) {
 
+        click.foreach((k, v) -> log.info("Click Key: {}, Value: {}", k, v));
+    }
 }
