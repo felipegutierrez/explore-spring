b26bd48f3a8337c52e46357f6e90503c8547b58d
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2021-04-02 13:11:48 +0200

    implemented binding and service for GlobalKTable to compute click from KStream using broadcast strategy [skip ci]

--- spring-kafka-stream/scripts/start.sh
+++ spring-kafka-stream/scripts/start.sh
@@ -108,8 +108,32 @@ USR101:{"UserID": "USR101","CreatedTime": "1549361940000","CurrentLink": "Previe
 100001:{"LoginID": "100001", "CreatedTime": 1550150291000}
 100087:{"LoginID": "100087", "CreatedTime": 1550150580000}
 
-#
-#
+# Kafka JOIN using GlobalKTable to process click using broadcast JOIN to a KStream
+# kafka-console-producer --broker-list localhost:9092 --topic active-inventories --property parse.key=true --property key.separator=":"
+# kafka-console-producer --broker-list localhost:9092 --topic ad-clicks --property parse.key=true --property key.separator=":"
+1001:{"InventoryID": "1001", "NewsType": "Sports"}
+1002:{"InventoryID": "1002", "NewsType": "Politics"}
+1003:{"InventoryID": "1003", "NewsType": "LocalNews"}
+1004:{"InventoryID": "1004", "NewsType": "WorldNews"}
+1005:{"InventoryID": "1005", "NewsType": "Health"}
+1006:{"InventoryID": "1006", "NewsType": "Lifestyle"}
+1007:{"InventoryID": "1007", "NewsType": "Literature"}
+1008:{"InventoryID": "1008", "NewsType": "Education"}
+1009:{"InventoryID": "1009", "NewsType": "Social"}
+1010:{"InventoryID": "1010", "NewsType": "Business"}
+
+1001:{"InventoryID": "1001"}
+1002:{"InventoryID": "1002"}
+1003:{"InventoryID": "1003"}
+1004:{"InventoryID": "1004"}
+1004:{"InventoryID": "1004"}
+1005:{"InventoryID": "1005"}
+1006:{"InventoryID": "1006"}
+1007:{"InventoryID": "1007"}
+1008:{"InventoryID": "1008"}
+1009:{"InventoryID": "1009"}
+1010:{"InventoryID": "1010"}
+
 #
 
 # Stop and delete all topics and data of confluent platform
--- spring-kafka-stream/src/main/java/com/github/felipegutierrez/explore/spring/services/ClickListenerService.java
+++ spring-kafka-stream/src/main/java/com/github/felipegutierrez/explore/spring/services/ClickListenerService.java
@@ -4,11 +4,14 @@ import com.github.felipegutierrez.explore.spring.bindings.ClicksListenerBinding;
 import com.github.felipegutierrez.explore.spring.model.AdClick;
 import com.github.felipegutierrez.explore.spring.model.AdInventories;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.kstream.GlobalKTable;
+import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.kafka.support.serializer.JsonSerde;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -20,5 +23,15 @@ public class ClickListenerService {
                         @Input("clicks-channel") KStream<String, AdClick> click) {
 
         click.foreach((k, v) -> log.info("Click Key: {}, Value: {}", k, v));
+
+        click.join(inventory,
+                (adClick, adClickValue) -> adClick,
+                (adClickValue, adInventories) -> adInventories)
+                .groupBy((joinedKey, joinedValue) -> joinedValue.getNewsType(),
+                        Grouped.with(Serdes.String(), new JsonSerde<>(AdInventories.class))
+                )
+                .count()
+                .toStream()
+                .foreach((k, v) -> log.info("Click Key: {}, Value: {}", k, v));
     }
 }
