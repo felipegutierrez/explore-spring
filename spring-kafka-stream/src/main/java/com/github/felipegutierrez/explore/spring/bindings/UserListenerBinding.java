b020d5ea7a384ec4e2a780eebd3e276c83175b23
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2021-04-02 12:19:24 +0200

    created binding and listener to KTable join for user click join operations [skip ci]

--- spring-kafka-stream/src/main/java/com/github/felipegutierrez/explore/spring/bindings/UserListenerBinding.java
+++ spring-kafka-stream/src/main/java/com/github/felipegutierrez/explore/spring/bindings/UserListenerBinding.java
@@ -1,4 +1,15 @@
 package com.github.felipegutierrez.explore.spring.bindings;
 
+import com.github.felipegutierrez.explore.spring.model.UserDetails;
+import com.github.felipegutierrez.explore.spring.model.UserLogin;
+import org.apache.kafka.streams.kstream.KTable;
+import org.springframework.cloud.stream.annotation.Input;
+
 public interface UserListenerBinding {
+
+    @Input("user-master-channel")
+    KTable<String, UserDetails> userInputStream();
+
+    @Input("user-login-channel")
+    KTable<String, UserLogin> loginInputStream();
 }
--- spring-kafka-stream/src/main/java/com/github/felipegutierrez/explore/spring/services/LoginListenerService.java
+++ spring-kafka-stream/src/main/java/com/github/felipegutierrez/explore/spring/services/LoginListenerService.java
@@ -1,12 +1,32 @@
 package com.github.felipegutierrez.explore.spring.services;
 
 import com.github.felipegutierrez.explore.spring.bindings.UserListenerBinding;
+import com.github.felipegutierrez.explore.spring.model.UserDetails;
+import com.github.felipegutierrez.explore.spring.model.UserLogin;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.streams.kstream.KTable;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.ZoneOffset;
 
 @Slf4j
 @Service
 @EnableBinding(UserListenerBinding.class)
 public class LoginListenerService {
+
+    @StreamListener
+    public void process(@Input("user-master-channel") KTable<String, UserDetails> users,
+                        @Input("user-login-channel") KTable<String, UserLogin> logins) {
+        users.toStream().foreach((k, v) -> log.info("User Key: {}, Last Login: {}, Value{}",
+                k, Instant.ofEpochMilli(v.getLastLogin()).atOffset(ZoneOffset.UTC), v));
+
+        logins.toStream().foreach((k, v) -> log.info("Login Key: {}, Last Login: {}, Value{}",
+                k, Instant.ofEpochMilli(v.getCreatedTime()).atOffset(ZoneOffset.UTC), v));
+        
+
+    }
 }
