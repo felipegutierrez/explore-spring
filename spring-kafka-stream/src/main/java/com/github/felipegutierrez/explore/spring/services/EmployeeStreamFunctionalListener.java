66a6f5243f7c4a162f0212dc12def8287ed97965
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2021-04-08 13:52:33 +0200

    implemented unit test with Mock schema registry for aggregate employees functional service to consume messages with AVRO serializer from Kafka topic [skip ci]

--- spring-kafka-stream/src/main/java/com/github/felipegutierrez/explore/spring/services/EmployeeStreamFunctionalListener.java
+++ spring-kafka-stream/src/main/java/com/github/felipegutierrez/explore/spring/services/EmployeeStreamFunctionalListener.java
@@ -3,16 +3,23 @@ package com.github.felipegutierrez.explore.spring.services;
 import com.github.felipegutierrez.explore.spring.model.DepartmentAggregate;
 import com.github.felipegutierrez.explore.spring.model.Employee;
 import com.github.felipegutierrez.explore.spring.utils.CustomSerdes;
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Materialized;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.apache.kafka.common.serialization.Serdes;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Map;
 import java.util.function.Function;
 
 @Slf4j
@@ -20,8 +27,30 @@ import java.util.function.Function;
 @EnableAutoConfiguration
 public class EmployeeStreamFunctionalListener {
 
+    private final Serde<String> stringSerde;
+    private final Serde<Employee> employeeSerde;
+    private final Serde<DepartmentAggregate> departmentAggregateSerde;
+
     @Autowired
     RecordBuilder recordBuilder;
+
+    public EmployeeStreamFunctionalListener(@Value("${application.configs.serdes.scope.test}") Boolean scopeTest) {
+        if (scopeTest) {
+            this.stringSerde = Serdes.String();
+            this.employeeSerde = new SpecificAvroSerde<>();
+            this.departmentAggregateSerde = new SpecificAvroSerde<>();
+
+            String SCHEMA_REGISTRY_SCOPE = EmployeeStreamFunctionalListener.class.getName();
+            String MOCK_SCHEMA_REGISTRY_URL = "mock://" + SCHEMA_REGISTRY_SCOPE;
+            Map<String, String> config = Map.of(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, MOCK_SCHEMA_REGISTRY_URL);
+            employeeSerde.configure(config, false);
+            departmentAggregateSerde.configure(config, false);
+        } else {
+            this.stringSerde = CustomSerdes.String();
+            this.employeeSerde = CustomSerdes.EmployeeAvro();
+            this.departmentAggregateSerde = CustomSerdes.DepartmentAggregateAvro();
+        }
+    }
 
     @Bean
     public Function<KStream<String, Employee>, KStream<String, DepartmentAggregate>> streamAggEmployeesByDepartment() {
@@ -30,13 +59,14 @@ public class EmployeeStreamFunctionalListener {
          * the subtractor operation from KTable. */
         return input -> input.peek((k, v) -> log.info("Employee - Key: {}, Value:{}", k, v))
                 .map((k, v) -> new KeyValue<>(v.getDepartment(), v))
-                .toTable(Materialized.with(CustomSerdes.String(), CustomSerdes.EmployeeAvro()))
-                .groupBy((k, v) -> KeyValue.pair(v.getDepartment(), v), Grouped.with(CustomSerdes.String(), CustomSerdes.EmployeeAvro()))
+                .map((k, v) -> new KeyValue<>(v.getDepartment(), v))
+                .toTable(Materialized.with(stringSerde, employeeSerde))
+                .groupBy((k, v) -> KeyValue.pair(v.getDepartment(), v), Grouped.with(stringSerde, employeeSerde))
                 .aggregate(
                         () -> recordBuilder.initDepartmentAggregate(),
                         (k, v, aggV) -> recordBuilder.addedDepartmentAggregate(v, aggV),
                         (k, v, aggV) -> recordBuilder.subtractDepartmentAggregate(v, aggV),
-                        Materialized.with(CustomSerdes.String(), CustomSerdes.DepartmentAggregateAvro())
+                        Materialized.with(stringSerde, departmentAggregateSerde)
                 )
                 .toStream()
                 .peek((k, v) -> log.info("DepartmentAggregate - Key: {}, Value:{}", k, v.toString()));
---
+++ spring-kafka-stream/src/test/java/intg/com/github/felipegutierrez/explore/spring/services/EmployeeStreamFunctionalListenerTest.java
@@ -0,0 +1,121 @@
+package com.github.felipegutierrez.explore.spring.services;
+
+import com.github.felipegutierrez.explore.spring.config.KafkaTopicsConfig;
+import com.github.felipegutierrez.explore.spring.model.DepartmentAggregate;
+import com.github.felipegutierrez.explore.spring.model.Employee;
+import io.confluent.kafka.schemaregistry.testutil.MockSchemaRegistry;
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.*;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Produced;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Map;
+import java.util.Properties;
+import java.util.function.Function;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE, properties = {"server.port=0"})
+@ExtendWith(SpringExtension.class)
+@DirtiesContext
+@EmbeddedKafka(partitions = 1)
+public class EmployeeStreamFunctionalListenerTest {
+
+    private static final String SCHEMA_REGISTRY_SCOPE = EmployeeStreamFunctionalListenerTest.class.getName();
+    private static final String MOCK_SCHEMA_REGISTRY_URL = "mock://" + SCHEMA_REGISTRY_SCOPE;
+    @Autowired
+    KafkaTopicsConfig kafkaTopicsConfig;
+    @Autowired
+    EmployeeStreamFunctionalListener employeeStreamFunctionalListener;
+    Serde<String> stringSerde = Serdes.String();
+    Serde<Employee> employeeSerde = new SpecificAvroSerde<>();
+    Serde<DepartmentAggregate> departmentAggregateSerde = new SpecificAvroSerde<>();
+    private TestInputTopic<String, Employee> employeeTopic;
+    private TestOutputTopic<String, DepartmentAggregate> departmentAggregateTopic;
+    private String INPUT_TOPIC;
+    private String OUTPUT_TOPIC;
+    private TopologyTestDriver testDriver;
+    private TestInputTopic inputTopic;
+    private TestOutputTopic outputTopic;
+
+    static Properties getStreamsConfiguration() {
+        final Properties streamsConfiguration = new Properties();
+        // Need to be set even these do not matter with TopologyTestDriver
+        streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "TopologyTestDriver");
+        streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "dummy:1234");
+        streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.StringSerde.class);
+        streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, SpecificAvroSerde.class);
+        streamsConfiguration.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, MOCK_SCHEMA_REGISTRY_URL);
+        return streamsConfiguration;
+    }
+
+    @BeforeEach
+    public void setUp() {
+        INPUT_TOPIC = kafkaTopicsConfig.getEMPLOYEE_FUNC_INPUT_TOPIC_NAME();
+        OUTPUT_TOPIC = kafkaTopicsConfig.getEMPLOYEE_FUNC_OUTPUT_TOPIC_NAME();
+
+        Map<String, String> config = Map.of(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, MOCK_SCHEMA_REGISTRY_URL);
+        employeeSerde.configure(config, false);
+        departmentAggregateSerde.configure(config, false);
+
+        final StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, Employee> input = builder.stream(INPUT_TOPIC, Consumed.with(stringSerde, employeeSerde));
+        final Function<KStream<String, Employee>, KStream<String, DepartmentAggregate>> process = employeeStreamFunctionalListener.streamAggEmployeesByDepartment();
+        final KStream<String, DepartmentAggregate> output = process.apply(input);
+        output.to(OUTPUT_TOPIC, Produced.with(stringSerde, departmentAggregateSerde));
+
+        testDriver = new TopologyTestDriver(builder.build(), getStreamsConfiguration());
+        inputTopic = testDriver.createInputTopic(INPUT_TOPIC, stringSerde.serializer(), employeeSerde.serializer());
+        outputTopic = testDriver.createOutputTopic(OUTPUT_TOPIC, stringSerde.deserializer(), departmentAggregateSerde.deserializer());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        testDriver.close();
+        MockSchemaRegistry.dropScope(SCHEMA_REGISTRY_SCOPE);
+    }
+
+    @Test
+    public void testOneEmployee() {
+        // employee: {"id": "101", "name": "Prashant", "department": "engineering", "salary": 5000}
+        Employee employee101 = Employee.newBuilder()
+                .setId("101")
+                .setName("Prashant")
+                .setDepartment("engineering")
+                .setSalary(5000)
+                .build();
+        inputTopic.pipeInput(employee101);
+
+        DepartmentAggregate departmentAggregateExpected = DepartmentAggregate.newBuilder()
+                .setEmployeeCount(1)
+                .setTotalSalary(5000)
+                .setAvgSalary(5000.0)
+                .build();
+
+        // Read and validate output
+        final DepartmentAggregate outputDepartmentAggregate = (DepartmentAggregate) outputTopic.readValue();
+        assertThat(outputDepartmentAggregate).isNotNull();
+
+        System.out.println("outputDepartmentAggregate");
+        System.out.println(outputDepartmentAggregate.getTotalSalary());
+        System.out.println(outputDepartmentAggregate.getEmployeeCount());
+        System.out.println(outputDepartmentAggregate.getAvgSalary());
+        assertThat(outputDepartmentAggregate).usingRecursiveComparison().isEqualTo(departmentAggregateExpected);
+
+        //No more output in topic
+        assertThat(outputTopic.isEmpty()).isTrue();
+    }
+}
