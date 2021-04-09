d71ca9119a03911af9f8db2b97137912247e396a
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2021-04-09 09:58:20 +0200

    created unit test for functional stream count window of simple invoices using Spring cloud + Kafka stream [skip ci]

--- spring-kafka-stream/src/main/java/com/github/felipegutierrez/explore/spring/services/InvoiceListenerFunctionalService.java
+++ spring-kafka-stream/src/main/java/com/github/felipegutierrez/explore/spring/services/InvoiceListenerFunctionalService.java
@@ -2,11 +2,17 @@ package com.github.felipegutierrez.explore.spring.services;
 
 import com.github.felipegutierrez.explore.spring.model.SimpleInvoice;
 import com.github.felipegutierrez.explore.spring.utils.CustomSerdes;
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
+import io.confluent.kafka.streams.serdes.json.KafkaJsonSchemaSerde;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.TimeWindows;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +20,7 @@ import org.springframework.context.annotation.Configuration;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.ZoneOffset;
+import java.util.Map;
 import java.util.function.Function;
 
 @Slf4j
@@ -21,19 +28,37 @@ import java.util.function.Function;
 @EnableAutoConfiguration
 public class InvoiceListenerFunctionalService {
 
+    private final Serde<String> stringSerde;
+    private final Serde<SimpleInvoice> simpleInvoiceSerde;
+
+    public InvoiceListenerFunctionalService(@Value("${application.configs.serdes.scope.test}") Boolean scopeTest) {
+        if (scopeTest) {
+            this.stringSerde = Serdes.String();
+            this.simpleInvoiceSerde = new KafkaJsonSchemaSerde<>();
+
+            String SCHEMA_REGISTRY_SCOPE = InvoiceListenerFunctionalService.class.getName();
+            String MOCK_SCHEMA_REGISTRY_URL = "mock://" + SCHEMA_REGISTRY_SCOPE;
+            Map<String, String> config = Map.of(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, MOCK_SCHEMA_REGISTRY_URL);
+            simpleInvoiceSerde.configure(config, false);
+        } else {
+            this.stringSerde = CustomSerdes.String();
+            this.simpleInvoiceSerde = CustomSerdes.SimpleInvoice();
+        }
+    }
+
     @Bean
     public Function<KStream<String, SimpleInvoice>, KStream<String, String>> streamSimpleInvoiceCountWindow() {
         return input -> input
                 .peek((k, v) -> log.info("Key = " + k + " Created Time = " + Instant.ofEpochMilli(v.getCreatedTime()).atOffset(ZoneOffset.UTC)))
-                .groupByKey(Grouped.with(CustomSerdes.String(), CustomSerdes.SimpleInvoice()))
-                .windowedBy(TimeWindows.of(Duration.ofSeconds(30)))
-                .count()
+                .groupByKey(Grouped.with(stringSerde, simpleInvoiceSerde))
+                .windowedBy(TimeWindows.of(Duration.ofSeconds(10)))
+                .count(Materialized.as("InvoiceCounts-1"))
                 .toStream()
                 .map((k, v) -> new KeyValue<>(k.key(),
-                                "StoreID: " + k.key() +
+                                "StoreID: " + k.key() + " Count: " + v +
                                         " Window start: " + Instant.ofEpochMilli(k.window().start()).atOffset(ZoneOffset.UTC) +
                                         " Window end: " + Instant.ofEpochMilli(k.window().end()).atOffset(ZoneOffset.UTC) +
-                                        " Count: " + v + " Window#: " + k.window().hashCode()
+                                        " Window#: " + k.window().hashCode()
                         )
                 );
     }
---
+++ spring-kafka-stream/src/test/java/intg/com/github/felipegutierrez/explore/spring/services/InvoiceListenerFunctionalServiceTest.java
@@ -0,0 +1,130 @@
+package com.github.felipegutierrez.explore.spring.services;
+
+import com.github.felipegutierrez.explore.spring.config.KafkaTopicsConfig;
+import com.github.felipegutierrez.explore.spring.model.SimpleInvoice;
+import io.confluent.kafka.schemaregistry.testutil.MockSchemaRegistry;
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
+import io.confluent.kafka.streams.serdes.json.KafkaJsonSchemaSerde;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.*;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Produced;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.support.serializer.JsonSerde;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Map;
+import java.util.Properties;
+import java.util.function.Function;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE, properties = {"server.port=0"})
+@ExtendWith(SpringExtension.class)
+@DirtiesContext
+@EmbeddedKafka(partitions = 1)
+public class InvoiceListenerFunctionalServiceTest {
+
+    private static final String SCHEMA_REGISTRY_SCOPE = EmployeeStreamFunctionalListenerTest.class.getName();
+    private static final String MOCK_SCHEMA_REGISTRY_URL = "mock://" + SCHEMA_REGISTRY_SCOPE;
+    @Autowired
+    KafkaTopicsConfig kafkaTopicsConfig;
+    @Autowired
+    InvoiceListenerFunctionalService invoiceListenerFunctionalService;
+    Serde<String> stringSerde = Serdes.String();
+    Serde<SimpleInvoice> simpleInvoiceSerde = new KafkaJsonSchemaSerde<>();
+    private TestInputTopic<String, SimpleInvoice> simpleInvoiceTopic;
+    private TestOutputTopic<String, String> outputTopic;
+    private String INPUT_TOPIC;
+    private String OUTPUT_TOPIC;
+    private TopologyTestDriver testDriver;
+
+    private SimpleInvoice simpleInvoice01;
+    private SimpleInvoice simpleInvoice02;
+    private SimpleInvoice simpleInvoice03;
+
+    static Properties getStreamsConfiguration() {
+        final Properties streamsConfiguration = new Properties();
+        // Need to be set even these do not matter with TopologyTestDriver
+        streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "TopologyTestDriver");
+        streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "dummy:1234");
+        streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.StringSerde.class);
+        streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, JsonSerde.class);
+        streamsConfiguration.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, MOCK_SCHEMA_REGISTRY_URL);
+        return streamsConfiguration;
+    }
+
+    @BeforeEach
+    public void setUp() {
+        // STR1534:{"InvoiceNumber": 101,"CreatedTime": "1549360860000","StoreID": "STR1534", "TotalAmount": 1920}
+        simpleInvoice01 = new SimpleInvoice();
+        simpleInvoice01.setInvoiceNumber("101");
+        simpleInvoice01.setCreatedTime(1549360860000l);
+        simpleInvoice01.setStoreID("STR1534");
+        simpleInvoice01.setTotalAmount(1920.0);
+
+        simpleInvoice02 = new SimpleInvoice();
+        simpleInvoice02.setInvoiceNumber("102");
+        simpleInvoice02.setCreatedTime(1549360900000l);
+        simpleInvoice02.setStoreID("STR1535");
+        simpleInvoice02.setTotalAmount(1860.0);
+
+        simpleInvoice03 = new SimpleInvoice();
+        simpleInvoice03.setInvoiceNumber("103");
+        simpleInvoice03.setCreatedTime(1549360999000l);
+        simpleInvoice03.setStoreID("STR1534");
+        simpleInvoice03.setTotalAmount(2400.0);
+
+        INPUT_TOPIC = kafkaTopicsConfig.getSIMPLE_INVOICE_FUNC_INPUT_TOPIC_NAME();
+        OUTPUT_TOPIC = kafkaTopicsConfig.getSIMPLE_INVOICE_FUNC_OUTPUT_TOPIC_NAME();
+
+        Map<String, String> config = Map.of(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, MOCK_SCHEMA_REGISTRY_URL);
+        simpleInvoiceSerde.configure(config, false);
+
+        final StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, SimpleInvoice> input = builder.stream(INPUT_TOPIC, Consumed.with(stringSerde, simpleInvoiceSerde));
+        final Function<KStream<String, SimpleInvoice>, KStream<String, String>> process = invoiceListenerFunctionalService.streamSimpleInvoiceCountWindow();
+        final KStream<String, String> output = process.apply(input);
+        output.to(OUTPUT_TOPIC, Produced.with(stringSerde, stringSerde));
+
+        testDriver = new TopologyTestDriver(builder.build(), getStreamsConfiguration());
+        simpleInvoiceTopic = testDriver.createInputTopic(INPUT_TOPIC, stringSerde.serializer(), simpleInvoiceSerde.serializer());
+        outputTopic = testDriver.createOutputTopic(OUTPUT_TOPIC, stringSerde.deserializer(), stringSerde.deserializer());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        testDriver.close();
+        MockSchemaRegistry.dropScope(SCHEMA_REGISTRY_SCOPE);
+    }
+
+    @Test
+    public void testOneInvoice() throws InterruptedException {
+        simpleInvoiceTopic.pipeInput(simpleInvoice01.getStoreID(), simpleInvoice01);
+
+        // Read and validate output
+        assertThat(outputTopic.isEmpty()).isFalse();
+        final String outputInvoiceWindow = outputTopic.readValue();
+        assertThat(outputInvoiceWindow).isNotNull();
+
+        System.out.println("outputInvoiceWindow");
+        System.out.println(outputInvoiceWindow);
+        String[] outputInvoiceWindowArray = outputInvoiceWindow.split(" ");
+        System.out.println(outputInvoiceWindowArray[1]);
+        System.out.println(outputInvoiceWindowArray[3]);
+        assertThat(outputInvoiceWindowArray[1]).isEqualTo(simpleInvoice01.getStoreID());
+        assertThat(outputInvoiceWindowArray[3]).isEqualTo("1");
+
+        //No more output in topic
+        assertThat(outputTopic.isEmpty()).isTrue();
+    }
+}
