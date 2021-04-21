c5653d0a95034027077de0705f9014d45385f4c4
Felipe Oliveira Gutierrez <felipe.o.gutierrez@gmail.com>
2021-04-21 09:34:40 +0200

    using VirtualTimeScheduler.getOrSet() [skip ci]

--- reactor-only/src/test/java/com/github/felipegutierrez/explore/basics/FluxAndMonoTransformationsTest.java
+++ reactor-only/src/test/java/com/github/felipegutierrez/explore/basics/FluxAndMonoTransformationsTest.java
@@ -5,7 +5,9 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.GroupedFlux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
+import reactor.test.scheduler.VirtualTimeScheduler;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -164,6 +166,35 @@ public class FluxAndMonoTransformationsTest {
     }
 
     @Test
+    void testFluxUsingConcatWithDelayWithVirtualTime() {
+
+        VirtualTimeScheduler.getOrSet();
+
+        Flux<String> concatFlux = fluxAndMonoTransformations.createFluxUsingConcatWithDelay(list1, list2);
+        List<String> expect = Stream.concat(list1.stream(), list2.stream()).collect(Collectors.toList());
+
+        StepVerifier.withVirtualTime(() -> concatFlux)
+                .thenAwait(Duration.ofSeconds(3))
+                .expectNextSequence(expect) // concat with delay DOES preserve order
+                .verifyComplete();
+    }
+
+    @Test
+    void testFluxUsingConcatWithDelayCountWithVirtualTime() {
+
+        VirtualTimeScheduler.getOrSet();
+
+        Flux<String> concatFlux = fluxAndMonoTransformations.createFluxUsingConcatWithDelay(list1, list2);
+        List<String> expect = Stream.concat(list1.stream(), list2.stream()).collect(Collectors.toList());
+
+        VirtualTimeScheduler.getOrSet();
+        StepVerifier.withVirtualTime(() -> concatFlux)
+                .thenAwait(Duration.ofSeconds(3))
+                .expectNextCount(expect.size()) // if we use expectNextSequence() we cannot use another expectNextCount
+                .verifyComplete();
+    }
+
+    @Test
     void testFluxUsingZip() {
         Flux<String> zipFlux = fluxAndMonoTransformations.createFluxUsingZip(list1, list2);
         List<String> expect = Arrays.asList("AD", "BE", "CF");
@@ -214,12 +245,42 @@ public class FluxAndMonoTransformationsTest {
     }
 
     @Test
+    void testFluxUsingGroupByWithVirtualTime() {
+
+        VirtualTimeScheduler.getOrSet();
+
+        int numberOfPartitions = 3;
+        int maxCount = 100;
+        Flux<GroupedFlux<Integer, Data>> dataGroupedFlux = fluxAndMonoTransformations.createFluxUsingGroupBy(expect, numberOfPartitions, maxCount);
+
+        StepVerifier.withVirtualTime(() -> dataGroupedFlux)
+                .thenAwait(Duration.ofSeconds(3))
+                .expectNextCount(numberOfPartitions)
+                .verifyComplete();
+    }
+
+    @Test
     void testFluxUsingHashGroupBy() {
         int numberOfPartitions = 3;
         int parallelism = 2;
         int maxCount = 100;
         Flux<GroupedFlux<Integer, Data>> dataGroupedFlux = fluxAndMonoTransformations.createFluxUsingHashGroupBy(expect, numberOfPartitions, parallelism, maxCount);
         StepVerifier.create(dataGroupedFlux)
+                .expectNextCount(numberOfPartitions)
+                .verifyComplete();
+    }
+
+    @Test
+    void testFluxUsingHashGroupByWithVirtualTime() {
+        VirtualTimeScheduler.getOrSet();
+
+        int numberOfPartitions = 3;
+        int parallelism = 2;
+        int maxCount = 100;
+        Flux<GroupedFlux<Integer, Data>> dataGroupedFlux = fluxAndMonoTransformations.createFluxUsingHashGroupBy(expect, numberOfPartitions, parallelism, maxCount);
+
+        StepVerifier.withVirtualTime(() -> dataGroupedFlux)
+                .thenAwait(Duration.ofSeconds(3))
                 .expectNextCount(numberOfPartitions)
                 .verifyComplete();
     }
