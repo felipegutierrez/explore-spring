0c3d503d5c6df5b26e241653a0eb08fd05e97652
Felipe Oliveira Gutierrez <felipe.o.gutierrez@gmail.com>
2021-04-21 16:22:45 +0200

    enabling swagger documentation [skip ci]

---
+++ spring-web-react-client/README.md
@@ -0,0 +1,8 @@
+
+
+## Swagger documentation
+
+ - JSON: [http://localhost:8081/v2/api-docs](http://localhost:8081/v2/api-docs)
+ - UI: [http://localhost:8081/swagger-ui/](http://localhost:8081/swagger-ui/)
+
+
--- spring-web-react-client/src/main/java/com/github/felipegutierrez/explore/spring/SpringWebReactClientApplication.java
+++ spring-web-react-client/src/main/java/com/github/felipegutierrez/explore/spring/SpringWebReactClientApplication.java
@@ -2,7 +2,9 @@ package com.github.felipegutierrez.explore.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
+@EnableWebFlux
 @SpringBootApplication
 public class SpringWebReactClientApplication {
 
