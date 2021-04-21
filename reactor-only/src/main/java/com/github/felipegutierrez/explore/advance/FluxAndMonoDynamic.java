a35588bca408cbc085abd432354131f10128ace8
Felipe Oliveira Gutierrez <felipe.o.gutierrez@gmail.com>
2021-04-21 13:26:08 +0200

    created Flux using FLux.create and asynch [skip ci]

--- reactor-only/src/main/java/com/github/felipegutierrez/explore/advance/FluxAndMonoDynamic.java
+++ reactor-only/src/main/java/com/github/felipegutierrez/explore/advance/FluxAndMonoDynamic.java
@@ -2,8 +2,16 @@ package com.github.felipegutierrez.explore.advance;
 
 import reactor.core.publisher.Flux;
 
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
 public class FluxAndMonoDynamic {
 
+    /**
+     * the generate operator is synchronous
+     *
+     * @return
+     */
     public Flux<Integer> generateFlux() {
         return Flux.generate(() -> 1, (state, integerSynchronousSink) -> {
             integerSynchronousSink.next(state * 3);
@@ -11,6 +19,19 @@ public class FluxAndMonoDynamic {
                 integerSynchronousSink.complete();
             }
             return state + 1;
+        });
+    }
+
+    public Flux<Integer> createAsyncFlux(List<String> names) {
+        return Flux.create(sink -> {
+            // synchronous
+            // names.stream().map(Integer::parseInt).forEach(sink::next);
+            // sink.complete();
+
+            // asynchronous
+            CompletableFuture.supplyAsync(() -> names)
+                    .thenAccept(n -> n.stream().map(Integer::parseInt).forEach(sink::next))
+                    .thenRun(sink::complete);
         });
     }
 }
--- reactor-only/src/test/java/com/github/felipegutierrez/explore/advance/FluxAndMonoDynamicTest.java
+++ reactor-only/src/test/java/com/github/felipegutierrez/explore/advance/FluxAndMonoDynamicTest.java
@@ -3,6 +3,8 @@ package com.github.felipegutierrez.explore.advance;
 import org.junit.jupiter.api.Test;
 import reactor.test.StepVerifier;
 
+import java.util.List;
+
 class FluxAndMonoDynamicTest {
 
     FluxAndMonoDynamic fluxAndMonoDynamic = new FluxAndMonoDynamic();
@@ -10,6 +12,15 @@ class FluxAndMonoDynamicTest {
     @Test
     void generateFlux() {
         var integerFlux = fluxAndMonoDynamic.generateFlux().log();
+        StepVerifier.create(integerFlux)
+                .expectNextCount(10)
+                .verifyComplete();
+    }
+
+    @Test
+    void createAsyncFlux() {
+        var list = List.of("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+        var integerFlux = fluxAndMonoDynamic.createAsyncFlux(list).log();
         StepVerifier.create(integerFlux)
                 .expectNextCount(10)
                 .verifyComplete();
