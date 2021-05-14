b30975a9771d0f55a05af7f44b7f08019b4f6aff
Felipe Gutierrez <felipe.o.gutierrez@gmail.com>
2021-05-14 17:52:09 +0200

    spring MVC async returning DeferredResult

--- spring-web/README.md
+++ spring-web/README.md
@@ -7,5 +7,6 @@
    mysql -h 127.0.0.1 -u user -P 3306 -p < spring-web/scripts/hplus_user.sql
    mysql -h 127.0.0.1 -u user -P 3306 -p < spring-web/scripts/hplus_product.sql
    ```
+ - Access: [http://localhost:8080/home](http://localhost:8080/home)
 
 
--- spring-web/src/main/java/com/github/felipegutierrez/explore/spring/controller/SearchController.java
+++ spring-web/src/main/java/com/github/felipegutierrez/explore/spring/controller/SearchController.java
@@ -3,10 +3,12 @@ package com.github.felipegutierrez.explore.spring.controller;
 import com.github.felipegutierrez.explore.spring.beans.Product;
 import com.github.felipegutierrez.explore.spring.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.async.DeferredResult;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
@@ -19,6 +21,11 @@ public class SearchController {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private AsyncTaskExecutor taskExecutor;
+
+    /** Handling exception asynchronously with java.util.concurrent.Callable */
+    /*
     @GetMapping("/search")
     public Callable<String> search(@RequestParam("search") String search, Model model, HttpServletRequest request) {
         System.out.println("in search controller");
@@ -27,12 +34,37 @@ public class SearchController {
         System.out.println("Thread from the servlet container: " + Thread.currentThread().getName());
 
         return () -> {
-            Thread.sleep(3000);
+            Thread.sleep(2000);
             System.out.println("Thread from the Spring MVC task executor: " + Thread.currentThread().getName());
             List<Product> products = new ArrayList<Product>();
             products = productRepository.searchByName(search);
             model.addAttribute("products", products);
             return "search";
         };
+    }
+     */
+
+    /** Handling exception asynchronously with org.springframework.web.context.request.async.DeferredResult */
+    @GetMapping("/search")
+    public DeferredResult<String> search(@RequestParam("search") String search, Model model, HttpServletRequest request) {
+        DeferredResult<String> deferredResult = new DeferredResult<>();
+        System.out.println("in search controller");
+        System.out.println("search criteria: " + search);
+        System.out.println("Async supported in application: " + request.isAsyncSupported());
+        System.out.println("Thread from the servlet container: " + Thread.currentThread().getName());
+
+        taskExecutor.execute(() -> {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Thread from the spring mvc task executor: " + Thread.currentThread().getName());
+            List<Product> products = new ArrayList<>();
+            products = productRepository.searchByName(search);
+            model.addAttribute("products", products);
+            deferredResult.setResult("search");
+        });
+        return deferredResult;
     }
 }
