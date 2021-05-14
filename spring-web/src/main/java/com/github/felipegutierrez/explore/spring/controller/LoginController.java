294ae56c97332696f0b419d52af3610421a8d56f
Felipe Gutierrez <felipe.o.gutierrez@gmail.com>
2021-05-14 17:06:21 +0200

    create exception handler for login user

--- spring-web/src/main/java/com/github/felipegutierrez/explore/spring/controller/LoginController.java
+++ spring-web/src/main/java/com/github/felipegutierrez/explore/spring/controller/LoginController.java
@@ -2,9 +2,11 @@ package com.github.felipegutierrez.explore.spring.controller;
 
 import com.github.felipegutierrez.explore.spring.beans.Login;
 import com.github.felipegutierrez.explore.spring.beans.User;
+import com.github.felipegutierrez.explore.spring.exceptions.ApplicationException;
 import com.github.felipegutierrez.explore.spring.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -17,6 +19,15 @@ public class LoginController {
     @PostMapping("/login")
     public String login(@ModelAttribute("login") Login login) {
         User user = userRepository.searchByName(login.getUsername());
+        if (user == null) {
+            throw new ApplicationException("User not found");
+        }
         return "search";
+    }
+
+    @ExceptionHandler(ApplicationException.class)
+    public String handleException() {
+        System.out.println("in exception handler of Login Controller");
+        return "error";
     }
 }
---
+++ spring-web/src/main/java/com/github/felipegutierrez/explore/spring/exceptions/ApplicationException.java
@@ -0,0 +1,9 @@
+package com.github.felipegutierrez.explore.spring.exceptions;
+
+public class ApplicationException extends RuntimeException {
+
+    public ApplicationException(String message) {
+        super(message);
+
+    }
+}
