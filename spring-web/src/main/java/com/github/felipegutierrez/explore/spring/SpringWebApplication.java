8c0fd48408f5999c64c04764780cdd65f78ae7f6
Felipe Gutierrez <felipe.o.gutierrez@gmail.com>
2021-05-14 09:55:18 +0200

    refactored application to generate war file

--- spring-web/src/main/java/com/github/felipegutierrez/explore/spring/SpringWebApplication.java
+++ spring-web/src/main/java/com/github/felipegutierrez/explore/spring/SpringWebApplication.java
@@ -1,19 +1,26 @@
 package com.github.felipegutierrez.explore.spring;
 
+import com.github.felipegutierrez.explore.spring.config.ApplicationConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 import java.util.Arrays;
 
-@SpringBootApplication
-public class SpringWebApplication {
+// @SpringBootApplication
+public class SpringWebApplication implements WebApplicationInitializer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SpringWebApplication.class);
 
-    public static void main(String[] args) {
+    // @SpringBootApplication
+    public static void main_old(String[] args) {
         ApplicationContext applicationContext =
                 SpringApplication.run(SpringWebApplication.class, args);
         LOGGER.info("use:");
@@ -27,5 +34,18 @@ public class SpringWebApplication {
         Arrays.stream(applicationContext.getBeanDefinitionNames()).forEach(name ->
                 LOGGER.info(name)
         );
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        // bootstrap the dispatcher servlet
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+
+        context.register(ApplicationConfig.class);
+
+        ServletRegistration.Dynamic servletRegistration = servletContext.addServlet("spring-mvc", new DispatcherServlet(context));
+        // using the eager initialization
+        servletRegistration.setLoadOnStartup(1);
+        servletRegistration.addMapping("/");
     }
 }
--- spring-web/src/test/java/com/github/felipegutierrez/explore/spring/SpringWebApplicationTests.java
+++ spring-web/src/test/java/com/github/felipegutierrez/explore/spring/SpringWebApplicationTests.java
@@ -3,7 +3,7 @@ package com.github.felipegutierrez.explore.spring;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class SpringWebApplicationTests {
 
 	@Test
