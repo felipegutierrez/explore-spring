7216b240bd294c80f149e3d8ab00387e6f18c464
Felipe Gutierrez <felipe.o.gutierrez@gmail.com>
2021-05-14 13:21:30 +0200

    :bug: fixed. JSP was not loading images and css file

--- spring-web/src/main/java/com/github/felipegutierrez/explore/spring/ApplicationConfig.java
+++ spring-web/src/main/java/com/github/felipegutierrez/explore/spring/ApplicationConfig.java
@@ -3,14 +3,12 @@ package com.github.felipegutierrez.explore.spring;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
-@EnableWebMvc
 @ComponentScan(basePackages = "com.github.felipegutierrez.explore.spring")
 public class ApplicationConfig extends WebMvcConfigurationSupport {
 
