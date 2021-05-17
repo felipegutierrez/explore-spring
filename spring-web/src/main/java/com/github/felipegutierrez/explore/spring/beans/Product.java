1450a61c6bff78eed69d9b3ccf8e6696359cdb06
Felipe Gutierrez <felipe.o.gutierrez@gmail.com>
2021-05-17 16:49:52 +0200

    created unit tests for search controller

--- spring-web/src/main/java/com/github/felipegutierrez/explore/spring/beans/Product.java
+++ spring-web/src/main/java/com/github/felipegutierrez/explore/spring/beans/Product.java
@@ -3,6 +3,7 @@ package com.github.felipegutierrez.explore.spring.beans;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -10,6 +11,7 @@ import javax.persistence.Id;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Accessors(chain = true)
 @Entity
 public class Product {
     @Id
--- spring-web/src/test/java/com/github/felipegutierrez/explore/spring/controller/SearchControllerTest.java
+++ spring-web/src/test/java/com/github/felipegutierrez/explore/spring/controller/SearchControllerTest.java
@@ -1,14 +1,20 @@
 package com.github.felipegutierrez.explore.spring.controller;
 
+import com.github.felipegutierrez.explore.spring.beans.Product;
 import com.github.felipegutierrez.explore.spring.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -25,19 +31,48 @@ class SearchControllerTest {
 
     @Test
     void searchCallable() throws Exception {
-        mockMvc.perform(get("/search/callable?search=water"))
-                .andExpect(status().isOk());
+
+        Product product = new Product().setId(1).setImagePath("path").setName("name");
+
+        Mockito.when(productRepository.searchByName(any()))
+                .thenReturn(List.of(product));
+
+        MvcResult result = mockMvc.perform(get("/search/callable?search=water"))
+                .andExpect(status().isOk())
+                .andReturn()
+                ;
     }
 
     @Test
     void searchDeferred() throws Exception {
-        mockMvc.perform(get("/search/deferred?search=water"))
-                .andExpect(status().isOk());
+        Product product = new Product().setId(1).setImagePath("path").setName("name");
+
+        Mockito.when(productRepository.searchByName(any()))
+                .thenReturn(List.of(product));
+
+        MvcResult result = mockMvc.perform(get("/search/deferred?search=water"))
+                .andExpect(status().isOk())
+                .andReturn()
+                ;
     }
 
     @Test
     void search() throws Exception {
-        mockMvc.perform(get("/search?search=water"))
-                .andExpect(status().isOk());
+
+        Product product = new Product().setId(1).setImagePath("path").setName("name");
+
+        Mockito.when(productRepository.searchByName(any()))
+                .thenReturn(List.of(product));
+
+        MvcResult result = mockMvc.perform(get("/search?search=water"))
+                .andExpect(status().isOk())
+                .andReturn()
+//                .andExpect(new ResultMatcher() {
+//                    @Override
+//                    public void match(MvcResult result) throws Exception {
+//                        System.out.println("content type: " + result.getResponse().getContentAsString());
+//                    }
+//                })
+                ;
     }
 }
