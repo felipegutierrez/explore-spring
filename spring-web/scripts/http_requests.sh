876fb1d9f5eada03eb03bb670a268c86de1e73dd
Felipe Gutierrez <felipe.o.gutierrez@gmail.com>
2021-05-17 13:37:48 +0200

    using @RestController and ResponseEntity

--- spring-web/scripts/http_requests.sh
+++ spring-web/scripts/http_requests.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 # curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X GET http://127.0.0.1:8080/hplus/rest/products | jq
+echo "1 - "
 curl -s GET http://127.0.0.1:8080/hplus/rest/products | jq '.'
 
+echo "2 - "
+curl -s GET http://127.0.0.1:8080/hplus/rest/product?name=water | jq '.'
+
+echo "3 - "
+curl -s GET http://127.0.0.1:8080/hplus/rest/product/water | jq '.'
--- spring-web/src/main/java/com/github/felipegutierrez/explore/spring/restcontrollers/ProductsRestController.java
+++ spring-web/src/main/java/com/github/felipegutierrez/explore/spring/restcontrollers/ProductsRestController.java
@@ -3,27 +3,40 @@ package com.github.felipegutierrez.explore.spring.restcontrollers;
 import com.github.felipegutierrez.explore.spring.beans.Product;
 import com.github.felipegutierrez.explore.spring.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
+// @Controller
+@RestController
 public class ProductsRestController {
 
     @Autowired
     private ProductRepository productRepository;
 
+    // @ResponseBody
     @GetMapping("/hplus/rest/products")
-    @ResponseBody
     public List<Product> getProducts() {
         //calll product repo
         List<Product> products = new ArrayList<>();
         productRepository.findAll().forEach(product -> products.add(product));
         return products;
 
+    }
+
+    @GetMapping("/hplus/rest/product")
+    public ResponseEntity getProductsByRequestParam(@RequestParam("name") String name){
+        List<Product> products = productRepository.searchByName(name);
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @GetMapping("/hplus/rest/product/{name}")
+    public ResponseEntity getProductsByPathVariable(@PathVariable("name") String name){
+        List<Product> products = productRepository.searchByName(name);
+        return new ResponseEntity<>(products, HttpStatus.OK);
     }
 }
