283a808a60209b7b3e33dfd6b81ada32ca1d9cac
Felipe Gutierrez <felipe.o.gutierrez@gmail.com>
2021-07-06 11:32:58 +0200

    created project for spring security and example of PKCE oAuth in spring

---
+++ spring-security/src/main/java/com/github/felipegutierrez/explore/spring/utils/PkceUtil.java
@@ -0,0 +1,29 @@
+package com.github.felipegutierrez.explore.spring.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class PkceUtil {
+
+    public String generateCodeVerifier() throws UnsupportedEncodingException {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] codeVerifier = new byte[32];
+        secureRandom.nextBytes(codeVerifier);
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(codeVerifier);
+    }
+
+    public String generateCodeChallange(String codeVerifier) throws UnsupportedEncodingException,
+            NoSuchAlgorithmException {
+        byte[] bytes = codeVerifier.getBytes(StandardCharsets.US_ASCII);
+        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+        messageDigest.update(bytes, 0, bytes.length);
+        byte[] digest = messageDigest.digest();
+
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(digest);
+    }
+
+}
